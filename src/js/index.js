fd9a1623e3fba55b3867fc430e5ca5f1dc121b2a
mchrzanka <mayachrzanowska@live.com>
2021-09-30 19:29:13 -0600

    app shell ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,2 @@
-
-const app = document.querySelector('#app');
-
-// React JSX
- 
- 
-
-app.innerHTML = `<h1>HOME PAGE </h1>`
+const app = document.querySelector('#app'); //grabs the div in index.html with ID app
+app.innerHTML = `<h1>HOME PAGE</h1>`; //template literal
